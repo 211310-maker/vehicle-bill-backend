572e37a7372ddef2667baa2b6ec455f12960db24
211310-maker <211310@juitsolan.in>
2025-11-30 05:05:33 +0530

    updated server.js
    Refactor server initialization to use dynamic PORT and connect to the database before starting the server.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-// server
+// server.js
 const app = require('./app');
-const { PORT } = require('./utils/constants');
 const connectDB = require('./utils/db');
 const logger = require('./logger');
 
-const server = app.listen(PORT, () => {
-  connectDB();
-  logger.info(`app listening at port ${PORT} in ${process.env.NODE_ENV} mode`);
-});
+// Render gives PORT dynamically
+const PORT = process.env.PORT || 5000;
 
-module.exports = server;
+connectDB();
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+});
